65d9a1c7b1dc5a2cb52f3f9dbd9fa5558d2f682d
Ambrose Alanda <alandaambrose@gmail.com>
2024-06-06 14:06:02 +0300

    send

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-    <title>Admin Dashboard</title>
+    <title>SkinScan Admin Dashboard</title>
     <link rel="shortcut icon" href="assets/img/favicon.png" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/plugins/feather/feather.css" />
     <link rel="stylesheet" href="assets/plugins/icons/flags/flags.css" />
@@ -17,12 +17,13 @@
 
 <body>
     <div class="main-wrapper">
+        <!-- Header -->
         <div class="header">
             <div class="header-left">
-                <a href="index.html" class="logo">
+                <a href="index.php" class="logo">
                     <img src="assets/img/logo.png" alt="Logo" />
                 </a>
-                <a href="index.html" class="logo logo-small">
+                <a href="index.php" class="logo logo-small">
                     <img src="assets/img/logo-small.png" alt="Logo" width="30" height="30" />
                 </a>
             </div>
@@ -31,7 +32,6 @@
                     <i class="fas fa-bars"></i>
                 </a>
             </div>
-
             <div class="top-nav-search">
                 <form>
                     <input type="text" class="form-control" placeholder="Search here" />
@@ -44,14 +44,15 @@
                 <i class="fas fa-bars"></i>
             </a>
 
+            <!-- Notifications -->
             <li class="nav-item dropdown noti-dropdown me-2">
                 <a href="#" class="dropdown-toggle nav-link header-nav-list" data-bs-toggle="dropdown">
-                    <img src="assets/img/icons/header-icon-05.svg" alt="" />
+                    <img src="assets/img/icons/notification-icon.svg" alt="Notifications" />
                 </a>
                 <div class="dropdown-menu notifications">
                     <div class="topnav-dropdown-header">
                         <span class="notification-title">Notifications</span>
-                        <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                        <a href="javascript:void(0)" class="clear-noti">Clear All</a>
                     </div>
                     <div class="noti-content">
                         <ul class="notification-list">
@@ -63,9 +64,8 @@
                                         </span>
                                         <div class="media-body flex-grow-1">
                                             <p class="noti-details">
-                                                <span class="noti-title">Carlson Tech</span> has
-                                                approved
-                                                <span class="noti-title">your estimate</span>
+                                                <span class="noti-title">Dr. Smith</span> reviewed
+                                                <span class="noti-title">your image upload</span>
                                             </p>
                                             <p class="noti-time">
                                                 <span class="notification-time">4 mins ago</span>
@@ -82,9 +82,8 @@
                                         </span>
                                         <div class="media-body flex-grow-1">
                                             <p class="noti-details">
-                                                <span class="noti-title">International Software Inc</span>
-                                                has sent you a invoice in the amount of
-                                                <span class="noti-title">$218</span>
+                                                <span class="noti-title">System</span> processed your scan for
+                                                <span class="noti-title">image IMG002</span>
                                             </p>
                                             <p class="noti-time">
                                                 <span class="notification-time">6 mins ago</span>
@@ -101,31 +100,11 @@
                                         </span>
                                         <div class="media-body flex-grow-1">
                                             <p class="noti-details">
-                                                <span class="noti-title">John Hendry</span> sent a
-                                                cancellation request
-                                                <span class="noti-title">Apple iPhone XR</span>
+                                                <span class="noti-title">Dr. Patel</span> commented on
+                                                <span class="noti-title">image IMG003</span>
                                             </p>
                                             <p class="noti-time">
                                                 <span class="notification-time">8 mins ago</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="notification-message">
-                                <a href="#">
-                                    <div class="media d-flex">
-                                        <span class="avatar avatar-sm flex-shrink-0">
-                                            <img class="avatar-img rounded-circle" alt="User Image" src="assets/img/profiles/avatar-13.jpg" />
-                                        </span>
-                                        <div class="media-body flex-grow-1">
-                                            <p class="noti-details">
-                                                <span class="noti-title">Mercury Software Inc</span>
-                                                added a new product
-                                                <span class="noti-title">Apple MacBook Pro</span>
-                                            </p>
-                                            <p class="noti-time">
-                                                <span class="notification-time">12 mins ago</span>
                                             </p>
                                         </div>
                                     </div>
@@ -139,18 +118,20 @@
                 </div>
             </li>
 
+            <!-- Fullscreen Mode -->
             <li class="nav-item zoom-screen me-2">
                 <a href="#" class="nav-link header-nav-list win-maximize">
-                    <img src="assets/img/icons/header-icon-04.svg" alt="" />
+                    <img src="assets/img/icons/fullscreen-icon.svg" alt="Fullscreen" />
                 </a>
             </li>
 
+            <!-- User Menu -->
             <li class="nav-item dropdown has-arrow new-user-menus">
                 <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
                     <span class="user-img">
-                        <img class="rounded-circle" src="assets/img/profiles/avatar-01.jpg" width="31" alt="Soeng Souy" />
+                        <img class="rounded-circle" src="assets/img/profiles/avatar-01.jpg" width="31" alt="Admin User" />
                         <div class="user-text">
-                            <h6>Soeng Souy</h6>
+                            <h6>Admin User</h6>
                             <p class="text-muted mb-0">Administrator</p>
                         </div>
                     </span>
@@ -161,22 +142,24 @@
                             <img src="assets/img/profiles/avatar-01.jpg" alt="User Image" class="avatar-img rounded-circle" />
                         </div>
                         <div class="user-text">
-                            <h6>Soeng Souy</h6>
+                            <h6>Admin User</h6>
                             <p class="text-muted mb-0">Administrator</p>
                         </div>
                     </div>
                     <a class="dropdown-item" href="profile.html">My Profile</a>
-                    <a class="dropdown-item" href="inbox.html">Inbox</a>
+                    <a class="dropdown-item" href="settings.html">Settings</a>
                     <a class="dropdown-item" href="login.html">Logout</a>
                 </div>
             </li>
-            </ul>
         </div>
+        <!-- /Header -->
 
+        <!-- Sidebar -->
         <div class="sidebar" id="sidebar">
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
                     <ul>
+                        <!-- Main Menu -->
                         <li class="menu-title">
                             <span>Main Menu</span>
                         </li>
@@ -185,56 +168,53 @@
                                 <span class="menu-arrow"></span></a>
                             <ul>
                                 <li>
-                                    <a href="index.html" class="active">Admin Dashboard</a>
+                                    <a href="index.php" class="active">Admin Dashboard</a>
                                 </li>
                                 <li>
                                     <a href="patient-dashboard.php">Patient Dashboard</a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="submenu">
-                            <a href="#"><i class="fas fa-graduation-cap"></i> <span> Users</span>
-                                <span class="menu-arrow"></span></a>
-                            <ul>
-                                <li><a href="users.php">User List</a></li>
-                                <li><a href="student-details.html">User View</a></li>
-                                <li><a href="add-student.html">User Add</a></li>
-                                <li><a href="edit-student.html">User Edit</a></li>
-                            </ul>
-                        </li>
 
+                        <!-- Management -->
                         <li class="menu-title">
                             <span>Management</span>
                         </li>
                         <li>
-                            <a href="scans.php"><i class="fas fa-clipboard-list"></i>
-                                <span>Scans</span></a>
-                        </li>
-
-                        <li>
-                            <a href="scan.php"><i class="fas fa-clipboard-list"></i>
-                                <span>Scan Upload</span></a>
+                            <a href="uploads.php"><i class="fas fa-upload"></i> <span>Upload Images</span></a>
                         </li>
                         <li>
-                            <a href="notifications.php"><i class="fas fa-calendar-day"></i>
-                                <span>Notification</span></a>
+                            <a href="scans.php"><i class="fas fa-clipboard-list"></i> <span>Manage Scans</span></a>
+                        </li>
+                        <li>
+                            <a href="uploads.php"><i class="fas fa-eye"></i> <span>Scan Status</span></a>
                         </li>
 
+                        <!-- Pages -->
                         <li class="menu-title">
                             <span>Pages</span>
                         </li>
+                        <li>
+                            <a href="profile.html"><i class="fas fa-user"></i> <span>Profile</span></a>
+                        </li>
+                        <li>
+                            <a href="notifications.php"><i class="fas fa-bell"></i> <span>Notifications</span></a>
+                        </li>
+
+                        <!-- Authentication -->
+                        <li class="menu-title">
+                            <span>Authentication</span>
+                        </li>
                         <li class="submenu">
-                            <a href="#"><i class="fas fa-shield-alt"></i>
-                                <span> Authentication </span> <span class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-shield-alt"></i> <span> Authentication </span> <span class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="login.html">Login</a></li>
                                 <li><a href="register.html">Register</a></li>
                                 <li><a href="forgot-password.html">Forgot Password</a></li>
-
                             </ul>
                         </li>
-
                     </ul>
                 </div>
             </div>
         </div>
+        <!-- /Sidebar -->
