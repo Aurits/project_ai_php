95a96894a7f413c0a42e02902ebcde1bab9f26ce
Ambrose Alanda <alandaambrose@gmail.com>
2024-06-07 03:52:52 +0300

    send

--- scans-grid.php
+++ scans-grid.php
@@ -4,22 +4,22 @@
         <div class="page-header">
             <div class="row align-items-center">
                 <div class="col">
-                    <h3 class="page-title">Teachers</h3>
+                    <h3 class="page-title">Scans</h3>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item">
                             <a href="index.html">Dashboard</a>
                         </li>
-                        <li class="breadcrumb-item active">Teachers</li>
+                        <li class="breadcrumb-item active">Scans</li>
                     </ul>
                 </div>
             </div>
         </div>
 
-        <div class="student-group-form">
+        <div class="scan-group-form">
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search by ID ..." />
+                        <input type="text" class="form-control" placeholder="Search by Scan ID ..." />
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
@@ -29,16 +29,17 @@
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search by Phone ..." />
+                        <input type="text" class="form-control" placeholder="Search by Type ..." />
                     </div>
                 </div>
                 <div class="col-lg-2">
-                    <div class="search-student-btn">
+                    <div class="search-scan-btn">
                         <button type="btn" class="btn btn-primary">Search</button>
                     </div>
                 </div>
             </div>
         </div>
+
         <div class="row">
             <div class="col-sm-12">
                 <div class="card card-table">
@@ -46,169 +47,59 @@
                         <div class="page-header">
                             <div class="row align-items-center">
                                 <div class="col">
-                                    <h3 class="page-title">Teachers</h3>
+                                    <h3 class="page-title">Scans</h3>
                                 </div>
                                 <div class="col-auto text-end float-end ms-auto download-grp">
-                                    <a href="teachers.html" class="btn btn-outline-gray me-2"><i class="feather-list"></i></a>
-                                    <a href="teachers-grid.html" class="btn btn-outline-gray me-2 active"><i class="feather-grid"></i></a>
+                                    <a href="scans.html" class="btn btn-outline-gray me-2"><i class="feather-list"></i></a>
+                                    <a href="scans-grid.html" class="btn btn-outline-gray me-2 active"><i class="feather-grid"></i></a>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="student-pro-list">
+                        <div class="scan-pro-list">
                             <div class="row">
                                 <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
                                     <div class="card">
                                         <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-06.jpg" />
+                                            <div class="scan-box flex-fill">
+                                                <div class="scan-img">
+                                                    <a href="scan-details.html">
+                                                        <img class="img-fluid" alt="Scan Thumbnail" src="assets/img/scans/scan-01.jpg" />
                                                     </a>
                                                 </div>
-                                                <div class="student-content pb-0">
+                                                <div class="scan-content pb-0">
                                                     <h5>
-                                                        <a href="teacher-details.html">Malynne</a>
+                                                        <a href="scan-details.html">Scan 101</a>
                                                     </h5>
-                                                    <h6>Teacher</h6>
+                                                    <h6>Type: X-Ray</h6>
+                                                    <p>Date: 2024-06-01</p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Repeat for other scan cards -->
                                 <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
                                     <div class="card">
                                         <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-04.jpg" />
+                                            <div class="scan-box flex-fill">
+                                                <div class="scan-img">
+                                                    <a href="scan-details.html">
+                                                        <img class="img-fluid" alt="Scan Thumbnail" src="assets/img/scans/scan-02.jpg" />
                                                     </a>
                                                 </div>
-                                                <div class="student-content pb-0">
+                                                <div class="scan-content pb-0">
                                                     <h5>
-                                                        <a href="teacher-details.html">Levell Scott</a>
+                                                        <a href="scan-details.html">Scan 102</a>
                                                     </h5>
-                                                    <h6>Teacher</h6>
+                                                    <h6>Type: MRI</h6>
+                                                    <p>Date: 2024-06-03</p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-03.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Minnie</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-02.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Lois A</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-08.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Calvin</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-09.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Charles Dickens</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-10.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Joe Kelley</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-4 col-md-6 d-flex">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="student-box flex-fill">
-                                                <div class="student-img">
-                                                    <a href="teacher-details.html">
-                                                        <img class="img-fluid" alt="Students Info" src="assets/img/profiles/avatar-08.jpg" />
-                                                    </a>
-                                                </div>
-                                                <div class="student-content pb-0">
-                                                    <h5>
-                                                        <a href="teacher-details.html">Charles Dickens</a>
-                                                    </h5>
-                                                    <h6>Teacher</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <!-- Continue for other scans -->
                             </div>
                         </div>
                     </div>
@@ -218,7 +109,7 @@
     </div>
 
     <footer>
-        <p>Copyright © 2022 Dreamguys.</p>
+        <p>Copyright © 2024 YourCompanyName.</p>
     </footer>
 </div>
 <?php include 'footer.php'; ?>
